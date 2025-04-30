dccdaa41d069c68e17ca12e63f3b05b91681a2c4
dugna1234@gmail.com <dugna1234@gmail.com>
2025-04-30 16:34:45 +0400

    feature: add swagger documentation for ease of use

--- src/asset_manager/urls.py
+++ src/asset_manager/urls.py
@@ -1,10 +1,23 @@
+from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 api_endpoint = 'api/v1/{endpoint}'
 
+schema = get_schema_view(
+    openapi.Info(
+        title = 'asset_manager',
+        default_version = 'v1.0.0',
+        description = 'An API which lets client agencies manage their assets.'
+    ),
+    permission_classes = (AllowAny,)
+)
+
 urlpatterns = [
+    path('', schema.with_ui(), name = 'swagger-docs'),
     path('admin/', admin.site.urls),
-    path(api_endpoint.format(endpoint = 'authentication'), 
+    path(api_endpoint.format(endpoint = 'authentication/'), 
          include('authentication.urls', namespace = 'authentication'))
 ]
