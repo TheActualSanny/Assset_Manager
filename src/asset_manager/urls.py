80171d0c9f19cbbd5d74fa6cbd730e9fe02b0106
dugna1234@gmail.com <dugna1234@gmail.com>
2025-05-01 16:32:13 +0400

    fix urls

--- src/asset_manager/urls.py
+++ src/asset_manager/urls.py
@@ -3,6 +3,18 @@ from django.urls import path, include
 
 api_endpoint = 'api/v1/{endpoint}'
 
+<<<<<<< Updated upstream
+=======
+schema = get_schema_view(
+    openapi.Info(
+        title = 'Asset Manager DOCS',
+        default_version = 'v1.0.0',
+        description = 'An API which lets client agencies manage their assets.'
+    ),
+    permission_classes = (AllowAny,)
+)
+
+>>>>>>> Stashed changes
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(api_endpoint.format(endpoint = 'authentication'), 
