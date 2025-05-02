c32d77e605ad2d7d89a7cc2cfffa5f7bd79f1157
dugna1234@gmail.com <dugna1234@gmail.com>
2025-05-02 17:38:33 +0400

    add redis cache to implement auto increment minio id

--- src/asset_manager/settings.py
+++ src/asset_manager/settings.py
@@ -118,6 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1', 
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH' : False,
