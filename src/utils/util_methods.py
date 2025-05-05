0d7de0a101c82c4a2fd9aeb7dacf4709f2f75017
dugna1234@gmail.com <dugna1234@gmail.com>
2025-05-05 18:06:01 +0400

    refactor: add another method for working with filenames

--- src/utils/util_methods.py
+++ src/utils/util_methods.py
@@ -13,3 +13,11 @@ def manage_incr() -> int:
         cache.set('id', str(int(val) + 1))
         return val
     
+def formatted_title(file_name: str) -> str:
+    '''
+        In managaer classes' methods where we
+        do lookups for certain assets, this method will be
+        called to compare the actual names instead of blob names.
+    '''
+    return file_name.split('_')[1]
+
