bcd21839946acd29795c1f31423b0a357493e5bc
dugna1234@gmail.com <dugna1234@gmail.com>
2025-05-12 17:43:36 +0400

    add config vars in settings.py and the worker in __init__

--- src/asset_manager/settings.py
+++ src/asset_manager/settings.py
@@ -169,3 +169,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
